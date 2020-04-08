c6976b3b222883d75426dcadde6379878310dd00
Enzo <enzobulacio18@gmail.com>
2020-04-07 23:10:04 -0300

    punto 3

--- tp2_2_1.cpp
+++ tp2_2_1.cpp
@@ -7,19 +7,28 @@ using namespace std;
 //15 filas columnas:entre 5 y 15
 int main(){
 	int columnas=0,y,x;
+	int cont_p,cont_i;
 	srand(time(NULL));
 	columnas=5 + rand()%(16-5);
 	int arreglo[15][columnas];
 	int *punt=&arreglo[0][0];
 	
 	for(x=0; x<filas; x++){
-		for(y=0; y<columnas; y++){//filas
-		
+		cont_p=0;cont_i=0;
+		for(y=0; y<columnas; y++){
 			*punt=100 + rand() %(1000-100);
 			printf("%4d",*punt);
-			}
-		printf("\n");
+			if(*punt % 2 ==0){
+				cont_p++;}
+			else{
+				cont_i++;}
 		}
+		
+		printf("   pares: %i",cont_p);
+		printf("   Impares: %i",cont_i);
+		printf("\n");
+	}
+	
 	getchar();
 	return 0;
 }
